123e3affec2f7130cf7efa4108a10eeed2342a8b
Roman Khadraliev <Romch33tos@yandex.ru>
2026-09-23 14:25:58 +0700

    feat: добавлен метод Свенна для поиска начального интервала

--- OneDimensionalOptimization/Program.cs
+++ OneDimensionalOptimization/Program.cs
@@ -21,6 +21,95 @@ namespace FunctionMinimization
       Console.WriteLine($"Заданная точность: {targetAccuracy}");
       Console.WriteLine($"Параметры Свенна: x0 = {initialPoint}, шаг = {stepSize}");
       Console.WriteLine();
+
+      try
+      {
+        var (intervalStart, intervalEnd) = SvennSearch(objectiveFunction, initialPoint, stepSize);
+        Console.WriteLine($"Начальный интервал неопределенности: [{intervalStart:F6}, {intervalEnd:F6}]");
+        Console.WriteLine();
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine($"Ошибка: {exception.Message}");
+      }
+    }
+
+    public static (double intervalStart, double intervalEnd) SvennSearch(Func<double, double> function, double initialPoint, double stepSize)
+    {
+      double leftPoint = initialPoint - stepSize;
+      double middlePoint = initialPoint;
+      double rightPoint = initialPoint + stepSize;
+
+      double leftValue = function(leftPoint);
+      double middleValue = function(middlePoint);
+      double rightValue = function(rightPoint);
+
+      if (leftValue >= middleValue && middleValue <= rightValue)
+      {
+        return (leftPoint, rightPoint);
+      }
+
+      if (leftValue <= middleValue && middleValue >= rightValue)
+      {
+        throw new InvalidOperationException("Функция не является унимодальной на заданном интервале.");
+      }
+
+      double searchDirection;
+      double intervalStart = 0;
+      double intervalEnd = 0;
+      double currentPoint;
+      double currentValue;
+
+      if (leftValue >= middleValue && middleValue >= rightValue)
+      {
+        searchDirection = stepSize;
+        intervalStart = initialPoint;
+        currentPoint = rightPoint;
+        currentValue = rightValue;
+      }
+      else
+      {
+        searchDirection = -stepSize;
+        intervalEnd = initialPoint;
+        currentPoint = leftPoint;
+        currentValue = leftValue;
+      }
+
+      int iterationIndex = 1;
+      while (true)
+      {
+        double nextPoint = currentPoint + Math.Pow(2, iterationIndex) * searchDirection;
+        double nextValue = function(nextPoint);
+
+        if (nextValue < currentValue)
+        {
+          if (searchDirection > 0)
+          {
+            intervalStart = currentPoint;
+          }
+          else
+          {
+            intervalEnd = currentPoint;
+          }
+          currentPoint = nextPoint;
+          currentValue = nextValue;
+          iterationIndex++;
+        }
+        else
+        {
+          if (searchDirection > 0)
+          {
+            intervalEnd = nextPoint;
+          }
+          else
+          {
+            intervalStart = nextPoint;
+          }
+          break;
+        }
+      }
+
+      return (intervalStart, intervalEnd);
     }
   }
 
